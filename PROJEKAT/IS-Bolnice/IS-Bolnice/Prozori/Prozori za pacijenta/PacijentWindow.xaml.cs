8f671ddbcfddcd1d5d12dca1d576df010f85ad8a
Bojan <lupulov.ra77.2018@uns.ac.rs>
2021-06-01 16:19:17 +0200

    Dodavanje akceleratora

--- PROJEKAT/IS-Bolnice/IS-Bolnice/Prozori/Prozori za pacijenta/PacijentWindow.xaml.cs
+++ PROJEKAT/IS-Bolnice/IS-Bolnice/Prozori/Prozori za pacijenta/PacijentWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using IS_Bolnice.Prozori;
 using IS_Bolnice.Prozori.Prozori_za_pacijenta;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace IS_Bolnice
 {
@@ -21,6 +10,7 @@ namespace IS_Bolnice
         public PacijentWindow()
         {
             InitializeComponent();
+
         }
 
         private void zakaziBtn_Click(object sender, RoutedEventArgs e)
@@ -67,47 +57,25 @@ namespace IS_Bolnice
             pto.ShowDialog();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            string[] items = imeKorisnika.Text.Split(); //items[2] jmbg
-
-            OdabirZakazivanja oz = new OdabirZakazivanja(items[2]);
-            oz.ShowDialog();
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private void izvestaj_Click(object sender, RoutedEventArgs e)
         {
             string[] items = imeKorisnika.Text.Split();
-            Obavestenje o = new Obavestenje(items[2]);
-            o.ShowDialog();
+            PrikazIzvestaja pi = new PrikazIzvestaja(items[2]);
+            pi.ShowDialog();
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void zakazivanjeKodLekara_Click(object sender, RoutedEventArgs e)
         {
             string[] items = imeKorisnika.Text.Split();
-            PacijentPregledZakazanihTermina pp = new PacijentPregledZakazanihTermina(items[2]);
-            pp.ShowDialog();
+            ZakazivanjeKodOdredjenogLekara zkol = new ZakazivanjeKodOdredjenogLekara(items[2]);
+            zkol.ShowDialog();
         }
-
-        private void Button_Click_3(object sender, RoutedEventArgs e)
-        {
-            MainWindow mw = new MainWindow();
-            mw.Show();
-            this.Close();
-        }
-
-        private void Button_Click_4(object sender, RoutedEventArgs e)
+        private void zakazivanjeUTerminu_Click(object sender, RoutedEventArgs e)
         {
             string[] items = imeKorisnika.Text.Split();
-            PrikazTerminaOperacija pto = new PrikazTerminaOperacija(items[2]);
-            pto.ShowDialog();
+            ZakazivanjeUOdredjenomTerminu ziot = new ZakazivanjeUOdredjenomTerminu(items[2]);
+            ziot.ShowDialog();
         }
 
-        private void Button_Click_5(object sender, RoutedEventArgs e)
-        {
-            string[] items = imeKorisnika.Text.Split();
-            FormaZaPopunjavanjeAnkete fzpa = new FormaZaPopunjavanjeAnkete(items[2]);
-            fzpa.ShowDialog();
-        }
     }
 }
