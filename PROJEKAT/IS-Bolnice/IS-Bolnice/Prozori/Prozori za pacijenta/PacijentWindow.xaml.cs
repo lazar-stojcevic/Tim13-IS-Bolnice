91d9e5510e9e6a805983f88602d1959fc3dc7124
Bojan <lupulov.ra77.2018@uns.ac.rs>
2021-06-01 14:05:03 +0200

    Uredjivanje osnovnog prozora pacijenta

--- PROJEKAT/IS-Bolnice/IS-Bolnice/Prozori/Prozori za pacijenta/PacijentWindow.xaml.cs
+++ PROJEKAT/IS-Bolnice/IS-Bolnice/Prozori/Prozori za pacijenta/PacijentWindow.xaml.cs
@@ -66,5 +66,48 @@ namespace IS_Bolnice
             PrikazTerminaOperacija pto = new PrikazTerminaOperacija(items[2]);
             pto.ShowDialog();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] items = imeKorisnika.Text.Split(); //items[2] jmbg
+
+            OdabirZakazivanja oz = new OdabirZakazivanja(items[2]);
+            oz.ShowDialog();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            string[] items = imeKorisnika.Text.Split();
+            Obavestenje o = new Obavestenje(items[2]);
+            o.ShowDialog();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string[] items = imeKorisnika.Text.Split();
+            PacijentPregledZakazanihTermina pp = new PacijentPregledZakazanihTermina(items[2]);
+            pp.ShowDialog();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            this.Close();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            string[] items = imeKorisnika.Text.Split();
+            PrikazTerminaOperacija pto = new PrikazTerminaOperacija(items[2]);
+            pto.ShowDialog();
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            string[] items = imeKorisnika.Text.Split();
+            FormaZaPopunjavanjeAnkete fzpa = new FormaZaPopunjavanjeAnkete(items[2]);
+            fzpa.ShowDialog();
+        }
     }
 }
